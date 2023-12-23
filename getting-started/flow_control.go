8af302b35161410411861e851d9f952aa5d3cf67
Lu Stormstout <jacklu.net@gmail.com>
2023-12-23 22:00:40 +0800

    Update flow_control.go

--- getting-started/flow_control.go
+++ getting-started/flow_control.go
@@ -1,25 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	// ------------------------------ if --------------------------------
 	var x int
-	fmt.Println("Please input a number:")
-	_, err := fmt.Scan(&x)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	x = rand.Intn(8)
 	if x >= 5 {
-		println("x is greater than 5")
+		println("x is greater than 5, x = ", x)
 	} else {
-		println("x is less than or equal to 5")
+		println("x is less than or equal to 5, x = ", x)
 	}
 
 	// ------------------------------ switch --------------------------------
+	//switch <var> {
+	//	case <val1>:
+	//		...
+	//	case <val2>:
+	//		...
+	//	default:
+	//		...
+	//}
+
+	// Go 语言的 switch 语句中，每个 case 语句的最后不需要加 break 关键字。
+	fmt.Println("--------------------")
+	switch x {
+	case 1:
+		fmt.Println("x = 1")
+	case 2:
+		fmt.Println("x = 2")
+	case 3:
+		fmt.Println("x = 3")
+	default:
+		fmt.Println("No matching case, x = ", x)
+	}
 
 	// ------------------------------ for --------------------------------
+	//for <init>; <condition>; <update> {
+	//	...
+	//}
+	fmt.Println("--------------------")
+	fmt.Println("for <init>; <condition>; <update> { ... }")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	//for <condition> {
+	//	...
+	//}
+	fmt.Println("--------------------")
+	fmt.Println("for <condition> { ... }")
+	i := 0
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	//for {
+	//	...
+	//}
+	fmt.Println("--------------------")
+	fmt.Println("for { ... }")
+	j := 0
+	for {
+		fmt.Println(j)
+		j++
+		if j > 10 {
+			break
+		}
+	}
 
 	// ------------------------------ goto --------------------------------
 
@@ -34,5 +86,5 @@ func main() {
 	// ------------------------------ panic --------------------------------
 
 	// ------------------------------ recover --------------------------------
-	
+
 }
