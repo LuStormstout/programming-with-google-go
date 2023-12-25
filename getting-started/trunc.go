94d6b1ba749d9276fc3e465216c8c067b23b0129
Lu Stormstout <jacklu.net@gmail.com>
2023-12-25 22:52:13 +0800

    Update trunc.go

--- getting-started/trunc.go
+++ getting-started/trunc.go
@@ -7,8 +7,8 @@ import (
 
 // 作业一：编写一个程序，获取一个浮点数的整数部分。
 func main() {
-	fmt.Println("Enter a floating point number:")
 	var x float64
+	fmt.Println("Enter a floating point number:")
 	_, err := fmt.Scan(&x)
 	if err != nil {
 		fmt.Println("Scan failed, err:", err)
